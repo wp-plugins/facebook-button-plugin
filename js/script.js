9b08bd6e033eb3c293eef8a9509efcce1628104a
bestwebsoft <bestwebsoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 08:08:52 +0000

    "V2.41 - Ability to use the current site language for Facebook buttons (Using Multilanguage by BestWebSoft). We fixed the 'share' button displaying bug (when the shortcode is used). "
    git-svn-id: https://plugins.svn.wordpress.org/facebook-button-plugin/tags/2.41@1200815 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script.js
@@ -0,0 +1,43 @@
+(function($) {
+	$(document).ready( function() {
+		$( '#fcbkbttn_settings_form input' ).bind( "change click select", function() {
+			if ( $( this ).attr( 'type' ) != 'submit' ) {
+				$( '.updated.fade' ).css( 'display', 'none' );
+				$( '#fcbkbttn_settings_notice' ).css( 'display', 'block' );
+			};
+		});
+		$( '#fcbkbttn_settings_form select' ).bind( "change", function() {
+			$( '.updated.fade' ).css( 'display', 'none' );
+			$( '#fcbkbttn_settings_notice' ).css( 'display', 'block' );
+		});
+
+		$( 'input[name="fcbkbttn_my_page"]' ).change( function() {
+			if ( $( this ).is( ":checked" ) ) {
+				$( '.fcbkbttn_my_page' ).show();
+				if ( $( 'select[name="fcbkbttn_display_option"]' ).val() == 'custom' ) {
+					$( '#fcbkbttn_display_option_custom' ).show();
+				} else {
+					$( '#fcbkbttn_display_option_custom' ).hide();
+				}
+			} else {
+				$( '.fcbkbttn_my_page' ).hide();
+			}
+		});
+
+		$( 'select[name="fcbkbttn_where"]' ).change( function() {
+			if ( $( this ).val() == 'shortcode' ) {
+				$( '#fcbkbttn_shortcode' ).show();
+			} else {
+				$( '#fcbkbttn_shortcode' ).hide();
+			}
+		});
+
+		$( 'select[name="fcbkbttn_display_option"]' ).change( function() {
+			if ( $( this ).val() == 'custom' ) {
+				$( '#fcbkbttn_display_option_custom' ).show();
+			} else {
+				$( '#fcbkbttn_display_option_custom' ).hide();
+			}
+		});
+	});
+})(jQuery);
